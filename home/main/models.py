e6dbb83daf3d8d8d939224bb3bc9dcfa857069f5
snezhanakisel <snezhok-88@mail.ru>
2021-09-18 00:02:32 +0300

    добавила модель Carusel и данные в словарь

--- home/main/models.py
+++ home/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Carusel(models.Model):
+    title = models.CharField('Заголовок', max_length=30)
+    subtitle = models.CharField('Подзаголовок', max_length=30)
+    image = models.ImageField('Картинка', upload_to='static')
+
+    def __str__(self):
+        return self.title
--- home/main/views.py
+++ home/main/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from .models import Carusel
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    carusel = Carusel.objects.all()
+    data = {
+        'carusels': carusel,
+        'construction': 'Construction',
+        'renovation': 'House Renovation',
+        'painting': 'Painting',
+        'design': 'Architecture Design',
+        'description': 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.',
+        'expert': 'Expert & Professional',
+        'work': 'High Quality Work',
+        'help': '24/7 Help Support',
+        'content': 'Separated they live in. A small river named Duden flows.',
+
+    }
+    return render(request, 'main/index.html', data)
