7ab864db6516fbf004eb1d05d4bf593a9d1d8792
snezhanakisel <snezhok-88@mail.ru>
2021-09-21 16:38:12 +0300

    добавила  модель Employee и поиск по полям name, position

--- home/team/admin.py
+++ home/team/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Employee
 
-# Register your models here.
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position')
+    search_fields = ('name', 'position')
+
+
+admin.site.register(Employee, EmployeeAdmin)
--- home/team/models.py
+++ home/team/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+
+    name = models.CharField('Имя', max_length=30)
+    position = models.CharField('Должность', max_length=30)
+    image = models.ImageField('Картинка', upload_to='static')
+    description = models.CharField('Описание', max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Сотрудник"
+        verbose_name_plural = "Сотрудники"
